7c925b9abe72f5bcdc389f18848bfff5a426ae1a
Tu Minh Duy <duytm2599@gmail.com>
2022-03-19 02:28:48 +0700

    History Order Custom Layout

--- app/src/main/java/com/ngoctin/intuitionmobile/activities/OrderHistoryActivity.java
+++ app/src/main/java/com/ngoctin/intuitionmobile/activities/OrderHistoryActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ngoctin.intuitionmobile.R;
 import com.ngoctin.intuitionmobile.adapter.OderHistoryAdapter;
@@ -16,6 +19,7 @@ public class OrderHistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ImageView btnBack = findViewById(R.id.btnBack);
         setContentView(R.layout.activity_order_history);
         RecyclerView orderHistoryRecyclerView = this.findViewById(R.id.rvOrderHistory);
         OderHistoryAdapter adapter = new OderHistoryAdapter(this);
@@ -25,5 +29,13 @@ public class OrderHistoryActivity extends AppCompatActivity {
                 ApplicationUtils.getJwt(this),
                 ApplicationUtils.getCurrentUserID(this),
                 orderHistoryRecyclerView,adapter);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OrderHistoryActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
